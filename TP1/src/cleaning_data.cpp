277b4652b7dd6a965ff10bb5ab51e6336657afdd
rfbr <fbr.romain@gmail.com>
2020-04-02 18:43:07 +0200

    Cleaning data correction

--- TP1/src/cleaning_data.cpp
+++ TP1/src/cleaning_data.cpp
@@ -10,7 +10,7 @@ int main(int argc,char** argv){
     }
     string temp = "awk '{if ($1<$2) print $1\" \"$2;else if ($2<$1) print $2\" \"$1}' ";
     temp += argv[1];
-    temp += " | sort -n -k1,2 -u > ";
+    temp += " | sort -n -k1 -k2 -u > ";
     temp += argv[2];
     char script[temp.size()+1];
     strcpy(script,temp.c_str());
